6cf25ab7aa46b601f5de760902fd649917e7f750
albertogmdev <albergonmar@gmail.com>
2025-08-21 10:57:01 +0100

    Remove users table

--- db_scripts/01_schema.sql
+++ db_scripts/01_schema.sql
@@ -5,12 +5,6 @@ DROP TABLE IF EXISTS Report;
 DROP TABLE IF EXISTS List;
 DROP TABLE IF EXISTS Task;
 
--- Create User
-CREATE TABLE User (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    email VARCHAR(255) NOT NULL,
-    password VARCHAR(255) NOT NULL
-);
 
 -- Create Schema
 CREATE TABLE Report (
