3b8e4a3d3a4ae8879e316a8b4facbef93b5ea377
albertogmdev <albergonmar@gmail.com>
2025-08-22 11:04:26 +0100

    Add redirect to reports listing

--- webapp/app/report/page.js
+++ webapp/app/report/page.js
@@ -1,18 +1,5 @@
-"use client"
+import { redirect } from 'next/navigation'
 
-import { useEffect, useState } from "react";
-
-export default function Home() {
-	const [data, setData] = useState(null)
-    const [error, setError] = useState(null)
-
-	useEffect(() => {
-		
-	}, [])
-
-	return (
-		<div className="">
-			<pre>{JSON.stringify(data)}</pre>
-		</div>
-	);
+export default function Report() {
+    redirect('/reports')
 }
