e9299ba675bfa1e43ed9ece0a7d4c0727e0abf3f
Alberto Gonzalez <albergonmar@gmail.com>
2025-08-29 01:10:09 +0200

    Create footer and add new icons

--- webapp/components/Footer/Footer.js
+++ webapp/components/Footer/Footer.js
@@ -1,7 +1,29 @@
 export default function Footer() {
     return (
         <footer className="footer">
-            
+            <div className="container">
+                <div className="footer-content">
+                    <p className="footer-title">Webscavul</p>
+                    <div className="footer-text">
+                        Aplicación desarrollada por&nbsp;
+                        <a 
+                            href="https://github.com/albertogmdev" 
+                            className="author-link" 
+                            target="_blank" 
+                            rel="noopener noreferrer">
+                            albertogmdev
+                        </a>.
+                    </div>
+                    <div className="footer-links">
+                        <a href="https://github.com/albertogmdev" className="footer-link" target="_blank" rel="noopener noreferrer">
+                            <span className="icon icon-github footer-icon" title="GitHub"></span>
+                        </a>
+                        <a href="https://www.linkedin.com/in/albertogonzalezmartinez" className="footer-link" target="_blank" rel="noopener noreferrer">
+                            <span className="icon icon-linkedin footer-icon" title="Linkedin"></span>
+                        </a>
+                    </div>
+                </div>
+            </div>
         </footer>
     );
 }
--- webapp/components/Header/Header.js
+++ webapp/components/Header/Header.js
@@ -11,7 +11,7 @@ export default function Header() {
 					Escaner
 				</Link>
 				<Link href="/reports" className="header-link">
-					Mis reportes
+					Mis informes
 				</Link>
 			</div>
 		</nav>
