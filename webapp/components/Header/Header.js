e877764c1b670e877b01bfe2b965e9eede2c12ea
Alberto Gonzalez <albergonmar@gmail.com>
2025-08-30 17:32:47 +0200

    Add theme switch functionality

--- webapp/components/Header/Header.js
+++ webapp/components/Header/Header.js
@@ -1,6 +1,19 @@
+"use client"
+
 import Link from 'next/link'
+import { useRef } from "react"
 
 export default function Header() {
+	const darkIcon = useRef(null)
+	const lightIcon = useRef(null)
+
+	const handleThemeToggle = () => {
+		const htmlElement = document.getElementsByTagName('html')[0]
+		htmlElement.classList.toggle('theme-light')
+		darkIcon.current.classList.toggle('hidden')
+		lightIcon.current.classList.toggle('hidden')
+	}
+		
 	return (
 		<nav id="header">
 			<Link href="/" className="header-logo">
@@ -13,8 +26,14 @@ export default function Header() {
 				<Link href="/reports" className="header-link">
 					Mis informes
 				</Link>
-				<button className="theme-button">
-					<span className="icon icon-light"></span>
+				<button 
+					id="theme-button"
+					className="theme-button"
+					title="Cambiar tema"
+					onClick={handleThemeToggle}
+				>
+					<span className="icon icon-light" ref={darkIcon}></span>
+					<span className="icon icon-dark hidden" ref={lightIcon}></span>
 				</button>
 			</div>
 		</nav>
